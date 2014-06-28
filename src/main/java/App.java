589b9c3e947a52a372dfcab22aee6c53aab498ae
Hao Wen <wenhao@126.com>
2014-06-28 13:55:44 +0800

    [MEMER][Wen Hao] - initial commit.

---
+++ src/main/java/App.java
@@ -0,0 +1,14 @@
+public class App
+{
+    private boolean junit;
+
+    public void setJunit(boolean junit)
+    {
+        this.junit = junit;
+    }
+
+    public boolean isJunit()
+    {
+        return junit;
+    }
+}
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class AppTest
+{
+    @Test
+    public void should_be_able_to_run_junit_test() throws Exception
+    {
+        // given
+        App app = new App();
+
+        // when
+        app.setJunit(true);
+
+        // then
+        assertThat(app.isJunit(), is(true));
+    }
+}
